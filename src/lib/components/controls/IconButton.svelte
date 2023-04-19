22669f5585953f36751c776c30d25a086b084507
Fergus Bentley <fergusbentley@gmail.com>
2023-04-19 10:00:52 +0100

    Factor out repeated code into components (#8)
    * Factor out repeated code into components
    
    * Tweak hero spacing on small screens
    
    ---------
    
    Co-authored-by: fergcb <fergus.bentley@gmail.com>

---
+++ src/lib/components/controls/IconButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { IconDefinition } from "@fortawesome/free-brands-svg-icons";
+	import Fa from "svelte-fa";
+
+  export let icon: IconDefinition;
+  export let size: 'md' | 'lg' | '2x' = '2x';
+  export let color = 'currentColor';
+  export let href: string | undefined = undefined;
+  export let external = false;
+</script>
+
+<svelte:element
+  this={href ? 'a' : 'button'}
+  {href}
+  target={href && external ? '_blank' : undefined}
+  class="flex items-center gap-2 text-fg-secondary bg-soft transition-colors duration-300 hover:text-fg-emphasis {$$props.class}"
+>
+  <Fa {icon} {size} {color} />
+  <slot />
+</svelte:element>
