43dba8c8ae875a3f88bba4a0de17ae797286a635
fergcb <fergus.bentley@gmail.com>
2023-05-08 16:46:40 +0100

    Add to docs intro

--- src/routes/docs/(pages)/+page.svelte
+++ src/routes/docs/(pages)/+page.svelte
@@ -1,11 +1,39 @@
 <script>
 	import Heading from "$lib/components/content/Heading.svelte";
 	import P from "$lib/components/content/P.svelte";
+	import Item from "$lib/components/content/list/Item.svelte";
+	import List from "$lib/components/content/list/List.svelte";
+	import IconButton from "$lib/components/controls/IconButton.svelte";
+	import Link from "$lib/components/controls/Link.svelte";
+	import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 </script>
 <Heading rank={1}>Introduction</Heading>
-<P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</P>
-<P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</P>
-<P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</P>
-<P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</P>
-<P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</P>
-<P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</P>
+<P><Link href="https://conlang.tools">conlang.tools</Link> is home to a cohesive toolset for creating believable constructed languages.</P>
+<P>Most of these tools are powered by Chronlang, a simple coding language that allows you to define sound changes using a syntax based on linguistic literature that should be familiar to conlangers and users of other popular sound change appliers.</P>
+
+<Heading rank={2}>Getting Started</Heading>
+<P>Start using the conlang workbench right away by following the quick-start guide.</P>
+<IconButton href="/docs/getting-started" placement="right" icon={faArrowRight}>Get Started</IconButton>
+
+<Heading rank={2}>Learn Chronlang</Heading>
+<P>Delve deeper into the features and syntax of the Chronlang definition language with the following articles:</P>
+<List>
+	<Item>
+		Fundamentals
+		<List>
+			<Item><Link href="/docs/languages">Languages & Language Families</Link></Item>
+			<Item><Link href="/docs/words">Words</Link></Item>
+			<Item><Link href="/docs/sound-changes">Sound Changes</Link></Item>
+			<Item><Link href="/docs/milestones">Milestones</Link></Item>
+		</List>
+	</Item>
+	<Item>
+		Advanced
+		<List>
+			<Item><Link href="/docs/imports">Imports</Link></Item>
+			<Item><Link href="/docs/traits">Traits</Link></Item>
+			<Item><Link href="/docs/classes">Classes</Link></Item>
+			<Item><Link href="/docs/series">Series</Link></Item>
+		</List>
+	</Item>
+</List>
